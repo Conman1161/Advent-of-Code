dd670efa904277594f44ba13cc701209e0d91c6f
Conman <conman1161@gmail.com>
2021-12-01 19:56:51 -0600

    Day 1 - 2015

---
+++ 2015/Day1/day1.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+
+
+let floor = 0;
+let input = fs.readFileSync(`${__dirname}\\input.txt`).toString();
+console.log(input);
+
+//part 1
+for (let i = 0; i < input.length; i++) {
+   if (input[i] == '(') {
+      floor += 1;
+   } else {
+      floor -= 1;
+   }
+}
+
+console.log(floor);
+
+//part 2
+floor = 0;
+
+for (let i = 0; i < input.length; i++) {
+   if (input[i] == '(') {
+      floor += 1;
+   } else {
+      floor -= 1;
+   }
+   if (floor == -1) {
+      console.log(i + 1);
+      break;
+   }
+}
+
+console.log(floor);
